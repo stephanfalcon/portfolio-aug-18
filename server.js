9940e913391b6439244f85bf1a9b39d604da92c0
stephan <hoobaloob99@gmail.com>
2020-02-05 14:41:06 -0700

    runs production build

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require("express")
-
-const port = process.env.PORT|| 3001
-
-const app = express()
-
-app.use(express.static("public"))
-
-app.get("/",(req,res)=>{
-    res.send("hello there")
-    console.log("hello there")
-})
-
-app.listen(port,()=> {
-    console.log(`listening on port ${port}`)
-})
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});app.listen(port);
