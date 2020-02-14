611931be060ebc1db3f63defb874a3fb8bf36e36
stephan <hoobaloob99@gmail.com>
2020-02-14 09:58:45 -0700

    i dont know

--- src/components/container/projects/projects.js
+++ src/components/container/projects/projects.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import Badges from "../badges/badge"
+import Button from "../button/button"
 import "./style.css"
 
 class Projects extends Component{
@@ -42,10 +43,12 @@ class Projects extends Component{
                     </div>
                 
                 </div>
-                
-                <div className={'row'}>
-                    <h3>Challenges</h3>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <div className={'row d-flex justify-content-center'}>
+                    {/* <h3>Challenges</h3>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+                    
+                    <Button title={'App'} className={'btn button btn-primary'}/> <Button title={'Code'} clasName={'btn button btn-primary'}/>
+                    
                 </div>
                 <hr></hr>
             </div>
